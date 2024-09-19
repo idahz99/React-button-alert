9dfaf252278cafd8562b79301a70805372ad9347
idahz99 <idahz99@gmail.com>
2024-09-19 10:15:49 +0800

    first

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Message from './message';
+
+function App(){
+  return <div><Message /></div>
+}
+
+export default App;
---
+++ vite-project/src/Message.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+export default function Message(){
+    const name = "Idah";
+    //Javascript XML
+return <h1>Hello World {name}</h1>;
+}
